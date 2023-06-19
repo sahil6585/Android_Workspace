01a905be5bfae9cd1c7702ab6df097dd5cb3ec98
sahil6585 <136334175+sahil6585@users.noreply.github.com>
2023-06-19 13:19:50 +0530

    Add files via upload

---
+++ conditions & loop statement  test/t10.kt
@@ -0,0 +1,22 @@
+fun main()
+{
+    println("enter number : ")
+    var num= readLine()!!.toInt()
+
+    var a=2
+    var b=0
+    while (a<=num/2)
+    {
+        if (num%a == 0)
+        {
+            println("NOT A PRIME NUMBER")
+            b = 1
+            break
+        }
+        a++
+    }
+    if (b==0)
+        {
+            println("PRIME NUMBER")
+        }
+}
