2bc7d81d84cd25673b6d43b60d7b572354da1d0a
sahil6585 <136334175+sahil6585@users.noreply.github.com>
2023-09-02 11:26:37 +0530

    Add files via upload

---
+++ SQLite_view_delete/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.sqlite_1
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.sqlite_1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    lateinit var dbHelper:DbHelper
+
+    @SuppressLint("SuspiciousIndentation")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        dbHelper= DbHelper(applicationContext)
+
+        binding.btn1.setOnClickListener {
+
+            var nm=binding.i1.text.toString()
+            var num=binding.i2.text.toString()
+
+            var mdl=Model()
+            mdl.name=nm
+            mdl.number=num
+
+        var id = dbHelper.insertdata(mdl)
+
+            Toast.makeText(applicationContext, "record inserted"+id, Toast.LENGTH_SHORT).show()
+        }
+        binding.btn2.setOnClickListener {
+            var i=Intent(applicationContext,MainActivity2::class.java)
+            startActivity(i)
+        }
+    }
+}
---
+++ SQLite_view_delete/MainActivity2.kt
@@ -0,0 +1,91 @@
+package com.example.sqlite_1
+
+import android.content.DialogInterface
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.ContextMenu
+import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView.AdapterContextMenuInfo
+import android.widget.SimpleAdapter
+import androidx.appcompat.app.AlertDialog
+import com.example.sqlite_1.databinding.ActivityMain2Binding
+
+class MainActivity2 : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMain2Binding
+    lateinit var dbHelper: DbHelper
+    lateinit var mutableList: MutableList<Model>
+    var arrayList: ArrayList<HashMap<String, String>> = ArrayList()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        dbHelper = DbHelper(applicationContext)
+        mutableList = ArrayList()
+
+        mutableList = dbHelper.viewdata()
+
+        for (i in mutableList) {
+            var hm = hashMapOf<String, String>()
+            hm["name"] = i.name
+            hm["number"] = i.number
+
+            arrayList.add(hm)
+        }
+        var fromarray = arrayOf("name", "number")
+        var toarray = intArrayOf(R.id.txt1, R.id.txt2)
+
+        var adapter =
+            SimpleAdapter(applicationContext, arrayList, R.layout.design, fromarray, toarray)
+        binding.lv.adapter = adapter
+
+        registerForContextMenu(binding.lv)
+    }
+
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        var m1 = menu!!.add(1, 1, 1, "UPDATE")
+        var m2 = menu!!.add(2, 2, 2, "DELETE")
+        super.onCreateContextMenu(menu, v, menuInfo)
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        var acm: AdapterContextMenuInfo = item.menuInfo as AdapterContextMenuInfo
+        var a = acm.position
+        var m: Model = mutableList.get(a)
+
+        when (item.itemId)
+        {
+            1->
+            {
+
+            }
+            2->
+            {
+                var alertDialog = AlertDialog.Builder(this)
+                alertDialog.setTitle("Are you sure you want to delete?")
+                alertDialog.setPositiveButton("YES", { dialogInterface: DialogInterface, i: Int ->
+
+                    dbHelper.deletedata(m.id)
+                    var i =Intent(applicationContext,MainActivity2::class.java)
+                    startActivity(i)
+                })
+                alertDialog.setNegativeButton("NO",{ dialogInterface: DialogInterface, i: Int ->
+
+                    dialogInterface.cancel()
+                })
+                alertDialog.show()
+
+            }
+
+        }
+            return super.onContextItemSelected(item)
+        }
+    }
