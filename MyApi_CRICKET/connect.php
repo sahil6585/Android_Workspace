400ea1b57b07a0df884f54761dea6a66d675ff38
sahil6585 <136334175+sahil6585@users.noreply.github.com>
2023-10-21 10:22:46 +0530

    Create connect.php

---
+++ MyApi_CRICKET/connect.php
@@ -0,0 +1,11 @@
+<?php
+
+    define('HOST','localhost');
+    define('USER','id21350448_tops');
+    define('PASS','Abcd?123');
+    define('DB','id21350448_practice');
+    
+    //variable -> $
+    $con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to connect');
+
+?>
